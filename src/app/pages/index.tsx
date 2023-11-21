d8f34b54a0e728d0e8e1d54be64133e132ac92be
leona <leonardovieiraxy@hotmail.com>
2023-11-21 18:33:41 -0300

    Changes to be committed: 	new file:   src/app/components/loginCard.tsx 	modified:   src/app/globals.css 	modified:   src/app/page.tsx 	new file:   src/app/pages/cadastroPage.tsx 	new file:   src/app/pages/index.tsx 	new file:   src/app/pages/loginPage.tsx 	new file:   src/models/Pessoa.tsx 	modified:   tsconfig.json

---
+++ src/app/pages/index.tsx
@@ -0,0 +1,10 @@
+import Login from "./loginPage";
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Login />
+    </div>
+  );
+}
---
+++ src/models/Pessoa.tsx
@@ -0,0 +1,9 @@
+export class Pessoa {
+  constructor(
+    public nome: string,
+    public endereco: string,
+    public cpf: string,
+    public sexo: string,
+    public dataNascimento: Date
+  ) {}
+}
